dadf1b4b2caceb90ca392b08132d7b141b151389
ColtonJP <colton.peterson06@gmail.com>
2018-11-09 16:16:06 -0800

    updated all pages css/html and worked on adding in different api

--- simple/civics/urls.py
+++ simple/civics/urls.py
@@ -6,10 +6,7 @@ from . import views
 app_name = 'civics'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('log_in/', views.login_page, name='log_in'),
-    path('logout_view/', views.logout_view, name='logout_view'),
-    path('register_user/', views.register_user, name='register_user'),
-    path('my_login/', views.my_login, name='my_login'),
+    path('about/', views.about, name='about'),
     path('detail/<state>/<house>', views.get_senator, name='get_senator'),
     path('detail/', views.get_senator, name='get_senator'),
     path('statement/', views.get_statement, name='get_statement'),
--- simple/civics/views.py
+++ simple/civics/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from .models import CurrentCongress
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 import requests
 
 
 def index(request):
     candidates = CurrentCongress.objects.all
     return render(request, 'civics/index.html', {'candidates': candidates})
+
+
+def about(request):
+    return render(request, 'civics/about.html')
 
 
 def get_senator(request, state, house):
@@ -21,14 +21,15 @@ def get_candidate(request):
     address = request.POST.get('address')
     state = request.POST.get('state1')
     incumbents = CurrentCongress.objects.filter(state=state)
-    r = requests.get("https://www.googleapis.com/civicinfo/v2/voterinfo?key=              &address="+address+""+state+"")
+    r = requests.get("https://www.googleapis.com/civicinfo/v2/voterinfo?key=&address="+address+""+state+"")
     if r.status_code == 200:
         data = r.json()['contests']
         return render(request, 'civics/candidate.html', {'data': data, 'incumbents': incumbents})
     else:
-        r = requests.get("https://www.googleapis.com/civicinfo/v2/representatives?key=              &address=" + address + "" + state + "")
-        data = r.json()['officials']
-        return render(request, 'civics/candidate.html', {'data': data, 'incumbents': incumbents})
+        r = requests.get("https://www.googleapis.com/civicinfo/v2/representatives?key=&address="+address+""+state+"")
+        currents = r.json()['officials']
+        print(currents)
+        return render(request, 'civics/candidate.html', {'currents': currents, 'incumbents': incumbents})
 
 
 def get_statement(request):
@@ -44,34 +45,5 @@ def get_statement(request):
         bills = r.json()['results']
         r = requests.get("https://api.open.fec.gov/v1/names/candidates/?api_key= &q=Patty%20Murray")
         contributions = r.json()
-        return render(request, 'civics/statement.html', {'bills': bills, 'data': data, 'candidate': candidate, 'contributions':contributions})
+        return render(request, 'civics/statement.html', {'bills': bills, 'data': data, 'candidate': candidate, 'contributions': contributions})
 
-
-def register_user(request):
-    username = request.POST['username']
-    email = request.POST['email']
-    password = request.POST['password']
-    user = User.objects.create_user(username, email, password)
-    login(request, user)
-    return HttpResponseRedirect(reverse('civics:index'))
-
-
-def login_page(request):
-    return render(request, 'civics/login.html')
-
-
-def my_login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect(reverse('civics:index'))
-    else:
-        return HttpResponseRedirect(reverse('civics:login'))
-
-
-@login_required(login_url='http://localhost:8000/civics/login/')
-def logout_view(request):
-    logout(request)
-    return render(request, 'civics/logout.html')
